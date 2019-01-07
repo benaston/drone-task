bae55a1d8b470ec634839584e7074b8f27881c3c
Ben Aston <501583+benaston@users.noreply.github.com>
2019-01-07 19:52:00 +0000

    Update client.js

--- drone-shack/client.js
+++ drone-shack/client.js
@@ -1,9 +1,9 @@
-import fetch = from "node-fetch";
-import retryOnceOnFail = from "/utils/retry-once-on-fail";
-import fromCacheOnFail = from "/redis/from-cache-on-fail";
-import toCacheOnSuccess = "/redis/to-cache-on-success";
-import toKey = from "./to-key";
-import is3rdPartyError = from "./is-3rd-party-error";
+import fetch from "node-fetch";
+import retryOnceOnFail from "/utils/retry-once-on-fail";
+import fromCacheOnFail from "/redis/from-cache-on-fail";
+import toCacheOnSuccess "/redis/to-cache-on-success";
+import toKey from "./to-key";
+import is3rdPartyError from "./is-3rd-party-error";
 
 // move to config
 const AUTHORITY = "https://www.unreliable-drone-api.com";
