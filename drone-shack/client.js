237a3f24e7e219eae1fda6d0f65c77de0cf2faa6
Shiva <shiva.razzaghkarimi@gmail.com>
2019-01-07 11:44:29 +0000

    Update client.js

--- drone-shack/client.js
+++ drone-shack/client.js
@@ -5,15 +5,16 @@ import toCacheOnSuccess = "/redis/to-cache-on-success";
 import toKey = from "./to-key";
 import is3rdPartyError = from "./is-3rd-party-error";
 
+// move to config
 const AUTHORITY = "https://www.unreliable-drone-api.com";
 
 // Proxy for API that looks like:
 // GET -> /api/v0/drones
 // GET -> /api/v0/drone/:id
-const get = (version, resource, id) =>
+const getData = (version, resource, id) =>
     fetch(`${AUTHORITY}/api/${version}/${resource}/${id}`)
     .then(res => res.json());
 
 export default {
-    get: retryOnceOnFail(fromCacheOnFail(toCacheOnSuccess(get, toKey), toKey, is3rdPartyError))
+    get: retryOnceOnFail(fromCacheOnFail(toCacheOnSuccess(getData, toKey), toKey, is3rdPartyError))
 };
