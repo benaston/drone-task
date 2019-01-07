1755dca1a22e1dfb95bb0f3e7c73b240c22ed2d5
Ben Aston <501583+benaston@users.noreply.github.com>
2019-01-07 09:50:48 +0000

    Update client.js

--- redis/client.js
+++ redis/client.js
@@ -1,11 +1,11 @@
-import redis = from "redis";
+import redis from "redis";
 
 const redisClient = redis.createClient(connectionString);
 const URL = process.env.REDIS_URL;
 
-const get = () =>
+const get = (key) =>
     new Promise((resolve, reject) =>
-        redisClient.get("schools/" + terms, (err, result) =>
+        redisClient.get(key, (err, result) =>
             err ? reject(err) : resolve(result)));
 
 const set = (...args) => redisClient.set(...args);
