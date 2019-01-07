7acab086bc913eb8078fa5de9cee381ce5772dec
Ben Aston <501583+benaston@users.noreply.github.com>
2019-01-07 21:00:12 +0000

    Update to-cache-on-success.js

--- redis/to-cache-on-success.js
+++ redis/to-cache-on-success.js
@@ -3,6 +3,7 @@ import cache from "./client";
 const LIFETIME_SECONDS = 300;
 
 // Wrap `fn` so that when it succeeds, the result 
-// is cached.
-export default (fn, toKey) => (...args) => fn(...args)
+// is cached. `toKey` is supplied as the logic to 
+// create the resource key for use in Redis.
+export default (toKey) => (fn) => (...args) => fn(...args)
     .then((result) => cache.setx(toKey(...args), LIFETIME_SECONDS, result), result);
