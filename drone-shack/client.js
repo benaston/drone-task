6881c16d08304a83990ec9fe135c4fe1ef06ffe8
Ben Aston <501583+benaston@users.noreply.github.com>
2019-01-07 21:21:50 +0000

    Update client.js

--- drone-shack/client.js
+++ drone-shack/client.js
@@ -1,9 +1,10 @@
 import fetch from "node-fetch";
-import retryOnceOnFail from "/utils/retry-once-on-fail";
-import fromCacheOnFail from "/redis/from-cache-on-fail";
-import toCacheOnSuccess "/redis/to-cache-on-success";
-import toKey from "./to-key";
+import compose from "/utils/compose";
+import useCacheOnFail from "/redis/use-cache-on-fail";
 import is3rdPartyError from "./is-3rd-party-error";
+import retryOnFail from "/utils/retry-on-fail";
+import cacheOnSuccess "/redis/cache-on-success";
+import toKey from "./to-key";
 
 // move to config
 const AUTHORITY = "https://www.unreliable-drone-api.com";
@@ -11,10 +12,15 @@ const AUTHORITY = "https://www.unreliable-drone-api.com";
 // Proxy for API that looks like:
 // GET -> /api/v0/drones
 // GET -> /api/v0/drone/:id
-const getData = (version, resource, id) =>
+const getResource = (version, resource, id) =>
     fetch(`${AUTHORITY}/api/${version}/${resource}/${id}`)
     .then(res => res.json());
 
+const get = compose(retryOnFail,
+                    useCacheOnFail(toKey, is3rdPartyError),
+                    cacheOnSuccess(toKey),
+                    getResource);
+
 export default {
-    get: retryOnceOnFail(fromCacheOnFail(toCacheOnSuccess(getData, toKey), toKey, is3rdPartyError))
+    get,
 };
