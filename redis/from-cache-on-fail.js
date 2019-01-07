06f78b3d42863a4b123e3157ba24c6e59af0fbc8
Ben Aston <501583+benaston@users.noreply.github.com>
2019-01-07 21:10:39 +0000

    Update from-cache-on-fail.js

--- redis/from-cache-on-fail.js
+++ redis/from-cache-on-fail.js
@@ -2,6 +2,6 @@ import cache from './client';
 
 // Wrap `fn` so that if it fails and `shouldUseCache`, 
 // an attempt is made to retrieve the value from the cache.
-export default (fn, toKey, shouldUseCache) => (...args) => fn(...args)
+export default (toKey, shouldUseCache) => (fn) => (...args) => fn(...args)
     .catch((err) => shouldUseCache(err) ?
         cache.get(toKey(...args)) : Promise.reject(err));
