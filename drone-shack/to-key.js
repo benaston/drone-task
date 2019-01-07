f916691c6860fd6fe0db7d29a41a1c88db68b58e
Ben Aston <501583+benaston@users.noreply.github.com>
2019-01-07 19:48:40 +0000

    Update to-key.js

--- drone-shack/to-key.js
+++ drone-shack/to-key.js
@@ -1,2 +1,2 @@
-// Converts a resource ID into a key for use in Redis.
+// Converts a resource version and ID into a key for use in Redis.
 export default (version, resource, id) => `/${version}/${resource}/${id}`;
