1825cb73cb81a5533953151871445e4bcdfa0524
Shiva <shiva.razzaghkarimi@gmail.com>
2019-01-11 22:03:43 +0000

    Update cache.mjs

--- drone-shack/resilient-client/cache.mjs
+++ drone-shack/resilient-client/cache.mjs
@@ -2,3 +2,5 @@ import client from '/redis/client';
 
 export default (p) => p
     .then(({ uri, json }) => client.set({ key: uri, value: json });
+          
+export default ({uri, json}) =>  client.set({ key: uri, value: json });         
